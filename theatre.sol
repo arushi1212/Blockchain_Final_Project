634ab1dfb6d99785350fedf25597aa68074ec683
giasareen12 <gsareen@purdue.edu>
2025-04-07 17:28:32 -0400

    testing

--- theatre.sol
+++ theatre.sol
@@ -1,1 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract MovieTheater {
+    address public owner;
+    uint public movieCount = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    //testing
+}
