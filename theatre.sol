7e1200d639f6b58cd4855b65e6a825c8dc5a63c0
giasareen12 <gsareen@purdue.edu>
2025-04-12 11:04:45 -0500

    Movie struct

--- theatre.sol
+++ theatre.sol
@@ -3,11 +3,26 @@ pragma solidity ^0.8.20;
 
 contract MovieTheater {
     address public owner;
-    uint public movieCount = 0;
+    uint256 public moviecount = 0;
 
     constructor() {
         owner = msg.sender;
     }
 
-    //testing
+    struct Movie {
+        string title;
+        uint256 price; 
+        uint256 totalseats;
+        uint256 availableseats;
+        uint256 showtime;
+        address[] ticketholders;
+        mapping(address => uint) ticketsbought;
+    }
+
+    mapping(uint => Movie) public movies;
+
+    modifier onlyowner() {
+        require(msg.sender == owner, "Only the admin can do this.");
+        _;
+    }
 }
